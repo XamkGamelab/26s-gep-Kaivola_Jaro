11d8012f7976316058cf76418190786a01726bce
JaroKaivola <144674385+JaroKaivola@users.noreply.github.com>
2026-09-10 22:06:15 +0300

    Initial

---
+++ GameEngineTeht/GameEngineTeht.cpp
@@ -0,0 +1,12 @@
+﻿// GameEngineTeht.cpp : Defines the entry point for the application.
+//
+
+#include "GameEngineTeht.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
